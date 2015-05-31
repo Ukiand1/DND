1e35cdeae03867b995f5e22aabc76979c737816d
Ukiand1 <ukiand13@gmail.com>
2015-05-31 20:49:05 +0200

    listFix

--- app/src/main/java/com/example/uros/dnd/Location.java
+++ app/src/main/java/com/example/uros/dnd/Location.java
@@ -9,21 +9,98 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Uros on 0025 25 May.
  */
 public class Location extends Activity{
 
-    ArrayList<String> types;
     ArrayList<String> animalsNameList;
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+    private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        ListView animalList=(ListView)findViewById(R.id.listViewAnimals);
+
+
+        animalsNameList = new ArrayList<String>();
+
+        animalsNameList.add("DOG");
+        animalsNameList.add("CAT");
+        animalsNameList.add("HORSE");
+        animalsNameList.add("ELEPHANT");
+        animalsNameList.add("LION");
+        animalsNameList.add("COW");
+        animalsNameList.add("MONKEY");
+        animalsNameList.add("DEER");
+        animalsNameList.add("RABBIT");
+        animalsNameList.add("BEER");
+        animalsNameList.add("DONKEY");
+        animalsNameList.add("LAMB");
+        animalsNameList.add("GOAT");
+
+        //getAnimalNames();
+
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,animalsNameList);
+        // Set The Adapter
+        animalList.setAdapter(arrayAdapter);
+
+        // register onClickListener to handle click events on each item
+        animalList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+            {
+
+                String selectedAnimal=animalsNameList.get(position);
+                Toast.makeText(getApplicationContext(), "Animal Selected : " + selectedAnimal, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+//    void getAnimalNames()
+//    {
+//        animalsNameList.add("DOG");
+//        animalsNameList.add("CAT");
+//        animalsNameList.add("HORSE");
+//        animalsNameList.add("ELEPHANT");
+//        animalsNameList.add("LION");
+//        animalsNameList.add("COW");
+//        animalsNameList.add("MONKEY");
+//        animalsNameList.add("DEER");
+//        animalsNameList.add("RABBIT");
+//        animalsNameList.add("BEER");
+//        animalsNameList.add("DONKEY");
+//        animalsNameList.add("LAMB");
+//        animalsNameList.add("GOAT");
+//
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
         // Get the reference of ListViewAnimals
-        ListView animalList=(ListView)findViewById(R.id.listaLocationView);
+        ListView animalList=(ListView)findViewById(R.id.listLocationView);
 
 
         animalsNameList = new ArrayList<String>();
@@ -66,5 +143,6 @@ public class Location extends Activity{
         animalsNameList.add("GOAT");
 
 
-    }
+    }*/
 }
+
